029c0c193a28e1fb9965e98c7b4b92c7dffa31ba
elnicomapero <75832950+elnicomapero@users.noreply.github.com>
2020-12-17 17:18:32 +0800

    Add actual initial functionality

--- your-friendly-animal.js
+++ your-friendly-animal.js
@@ -6,13 +6,28 @@ const actions = ['sleeps', 'shouts', 'stings', 'runs', 'sings', 'climbs', 'laugh
 
 const affinity = ['a great friend to', 'an inseparable friend to', 'a fearful enemy for', 'a completely indifferent species towards', 'the worst enemy for', 'scared of', 'amused by', 'funny for', 'a great imitator of', 'a role model for'];
 
-const ourAnimal = {
-    animal: 'animal',
-    character: 'adjective',
-    habit: 'action',
-    affinity: 'affinity'
+// A function that selects a random element of an array. This allows to add, remove or modify arrays. 
+const randomize = arr => {
+    const randomNum = Math.floor(Math.random() * arr.length);
+    return arr[randomNum];
 };
 
-let message = `The ${ourAnimal.animal} is a very ${ourAnimal.character} animal which ${ourAnimal.habit} a lot and, in turn, it tends to be ${ourAnimal.affinity} humans.`;
+// This functions creates a random custom animal baing on each of the attributes (arrays) passed as args. The returning object should be customized should we add or remove arrays.
+const makeAnimal = (animal, character, habit, affinity) => {
+    const myAnimal = {
+        animal: randomize(animal),
+        character: randomize(character),
+        habit: randomize(habit),
+        affinity: randomize(affinity),
+    };
+    return myAnimal;
+};
 
+// Creates a specific instance of an animal.
+const myAnimal = makeAnimal(animals, adjectives, actions, affinity)
+
+// The final message, That hardcoded part should be customized every time an array is added or removed.
+const message = `The ${myAnimal.animal} is a very ${myAnimal.character} animal which ${myAnimal.habit} a lot and, in turn, it tends to be ${myAnimal.affinity} humans.`;
+
+// Logs message to the console.
 console.log(message);
